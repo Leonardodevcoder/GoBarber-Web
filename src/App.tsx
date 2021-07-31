df34b3a33d6e9b3d25fc011ba351c10d84c8b6fe
Leonardodevcoder <leonardoluan2017@gmail.com>
2021-07-30 22:53:45 -0300

    fix: validando cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/globals';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <SignUp />
       <GlobalStyle />
     </>
   );
 };
-
 export default App;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,20 +10,31 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('E-mail obrigatório'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-
         <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
-
           <Input name="name" icon={FiUser} placeholder="E-mail" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
@@ -31,10 +43,8 @@ const SignUp: React.FC = () => {
             type="password"
             placeholder="Senha"
           />
-
           <Button type="submit">Cadastrar</Button>
         </Form>
-
         <a href="signup">
           <FiArrowLeft />
           Voltar para logon
