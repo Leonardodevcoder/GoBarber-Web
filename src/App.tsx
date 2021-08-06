cf3a9599d59ea4588fa683c9015726420dac6e06
Leonardodevcoder <leonardoluan2017@gmail.com>
2021-08-05 21:02:50 -0300

    Configurando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import GlobalStyle from './styles/globals';
 
 import AppProvider from './hooks';
 
+import Routes from './routes';
+
 const App: React.FC = () => {
   return (
-    <>
+    <Router>
       <AppProvider>
-        <SignIn />
+        <Routes />
       </AppProvider>
 
       <GlobalStyle />
-    </>
+    </Router>
   );
 };
+
 export default App;
